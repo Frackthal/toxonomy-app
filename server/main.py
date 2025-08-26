11f05d7be3849859823c5e2cbb0504bec4bfc299
Frackthal <61191174+Frackthal@users.noreply.github.com>
2025-08-26 16:49:51 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -45,8 +45,8 @@ SPECIAL_CARCINOGENICITY = {
     ],
     "NTP_Carcinogens": lambda row: dict(row).get("Listing") and str(row["Listing"]).strip() != "",
     "MAK_Carcinogens": lambda row: dict(row).get("Category") and str(row["Category"]).strip() != "",
-    "ACGIH": lambda row: any(x in str(row.get("Notation", "")).upper() for x in ["A1", "A2", "A3"],
-    "OEHHA": lambda row: "cancer" in str(row.get("Toxicity", "")).lower())
+    "ACGIH": lambda row: any((x in str(row.get("Notation", "")).upper()) for x in ["A1", "A2", "A3"]),
+    "OEHHA": lambda row: "cancer" in str(row.get("Toxicity", "")).lower(),
 }
 
 def normalize_cas(cas):
